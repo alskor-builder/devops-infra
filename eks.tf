76539d34b0422e776231ce50d3e717ff72e42335
akiokussu <skorovalex@gmail.com>
2024-06-23 01:40:24 +0300

    added kube provider

--- eks.tf
+++ eks.tf
@@ -28,13 +28,13 @@ module "eks" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks_blueprints.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+  host                   = module.eks.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority_data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
+    args = ["eks", "get-token", "--cluster-name", module.eks.eks_cluster_id]
   }
 }
